3abcbd86476dc09e32d7ca3ddf8f2cdf7eae8716
Tyler McCann <tyler@proxbox.dev>
2026-01-07 11:55:45 -0600

    Fixed yet another typo.

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,7 @@ Import-Module Terminal-Icons
 
 # Load any PS1 file in designated folder into Session
 $ProfileDir = "${env:USERPROFILE}/Documents/PowerShell/PROFILE"
-(Get-ChildItem $ProfileDir -Filter '*.ps1').Fullname) | % { . $_ }
+(Get-ChildItem $ProfileDir -Filter '*.ps1').Fullname | % { . $_ }
 
 # General Quality of Life (QoL) Features
 function Get-PublicIP { Invoke-RestMethod https://ifconfig.me/ip }
